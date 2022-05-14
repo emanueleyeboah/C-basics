4df64bc7c9676c058f37e4663d36ceadcaa40d1f
Emmanuel Yeboah <emanueleyeboah@gmail.com>
2022-05-14 22:35:55 +0000

    Add C basics

---
+++ variable.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main ()
+{
+	// Variable is a name of the memory location.
+	// It is used to store data
+	// Types of variables.. 1.Int(Integer) 2. Float(Floating-point)
+	// 3. Char (Character)
+	
+	// We focus on basic addition, subraction, multiplication and division iin c.
+	//Addition
+	
+	int x = 2000 ;
+	int y = 230;
+	int z = 200 ;
+
+	printf("Addition\n");
+	int add = x + y + z ;
+
+	printf("Answer: %i\n", add);
+	
+	printf("\n");
+
+	//Substraction
+	
+	printf("Substraction\n");
+	int sub = x - y - z ;
+	printf("Answer : %i\n", sub);
+
+	printf("\n");
+
+
+	//Multiplication
+	
+	printf("Multiplication\n");
+	int mul = x * y * z ;
+	printf ("Answer : %i\n", mul);
+
+	printf("\n");
+
+	//Division
+	
+	printf("Division\n");
+	int div = x / y / z ;
+	printf ("Anwer: %i\n", div);
+	
+
+
+	
+}
