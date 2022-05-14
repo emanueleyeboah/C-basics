f3fd769860078b760b9fbb5d4928bbb75220daae
Emmanuel Yeboah <emanueleyeboah@gmail.com>
2022-05-14 22:27:45 +0000

    Add basics

---
+++ whileloop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int main()
+{
+      //let i = 0
+	int i =0;
+	
+	// while i is  less than 10
+	while (i<10)
+	{
+	
+		// add 1 to i that is i = i +1
+		// break when you get to 10
+		i ++;
+		printf("i = %d\n",i);
+	}
+}
