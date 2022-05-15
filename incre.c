5b59a3d79d0c3360ea24d65b52ae75dcff85a75f
Emmanuel Yeboah <emanueleyeboah@gmail.com>
2022-05-15 22:16:12 +0000

    Add  C basic

---
+++ incre.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/*  ++ is the increment operator
+    increment operator increases the value of a variable by 1 
+    x ++ is equivalent to x = x + 1 
+    */
+
+/* is the decrement operator
+   Decrement operator decreases the value of a variable by 1
+   y -- is equivalent to y = y-1*/
+
+
+int main()
+
+{
+	// i = i +1
+	
+	int i = 10;
+	i ++;
+	printf("\n%d\n", i);
+
+	// k = k+1
+	
+	float k = 23.7;
+	k ++;
+	printf("\n%f\n",k);
+
+	// m = m-1
+	char m ='C';
+	m--;
+	printf("\n%c\n",m);
+}
+
